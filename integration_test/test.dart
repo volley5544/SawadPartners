f3a99c9f27cb44d9c41eba019a4c0d512c1f0419
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-08-01 04:56:40 +0000

    Updating to latest FlutterFlow output.

--- integration_test/test.dart
+++ integration_test/test.dart
@@ -36,7 +36,9 @@ void main() async {
 
     await tester.pumpWidget(ChangeNotifierProvider(
       create: (context) => FFAppState(),
-      child: const MyApp(),
+      child: MyApp(
+        entryPage: LoginWidget(),
+      ),
     ));
     await GoogleFonts.pendingFonts();
 
